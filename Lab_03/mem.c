8509aea194f88f8b2463e409e3ff08c39d402a1c
Jatnael Montes <ifelsejet@gmail.com>
2020-09-23 13:59:59 -0700

    Lab 3 Progress

---
+++ Lab_03/mem.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+ int main() {
+	int num;
+	int *ptr;
+	int **handle;
+
+	num = 14;
+	ptr = (int *)malloc(2 * sizeof(int));
+	*ptr = num;
+	handle = (int **)malloc(1 * sizeof(int *));
+	*handle = ptr;
+
+	// Insert extra code here
+	printf("addres of where the ptr is pointing %p \n", ptr);
+	printf("address of where the handle is pointing to %p \n", handle);
+
+
+	return 0;
+} 
+
