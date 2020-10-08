f0db95e008bdf0ce45b0880e340592023589836f
Jatnael Montes <ifelsejet@gmail.com>
2020-10-07 21:04:47 -0700

    Create myFirstMIPS.s

---
+++ Lab_05/myFirstMIPS.s
@@ -0,0 +1,22 @@
+.data
+
+
+n:      .word 13
+
+        .text
+main: 		
+		
+		add     $t0, $s0, $zero
+		addi    $t1, $t0, -1
+		addi    $t2, $t1, -2
+		addi    $t3, $t2, -3
+		addi    $t4, $t3, -4
+		addi    $t5, $t4, -5
+		addi    $t6, $t5, -6
+		addi    $t7, $t6, -7
+
+finish: addi    $a0, $t7, 0
+		li      $v0, 1
+		syscall			
+		li      $v0, 10		#Print output here
+		syscall			
